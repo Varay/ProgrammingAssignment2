3652e6c33528f7f3b5a627514112714d40234da6
Varay <Varay@users.noreply.github.com>
2014-07-24 10:44:26 +0200

    solution for 'Programming Assignment 2'

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,69 @@
-## Put comments here that give an overall description of what your
-## functions do
+#Functions for calculating inversion of the matrix smart way (lazy evaluation + caching)
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+#'makeCacheMatrix' creates special matrix (list of functions) that is able to cache result of 'solve' function
+#parameters:
+#   currentMatrix - matrix to store
+#result:
+#   special matrix (to be used as a parameter for 'cacheSolve' function)
+makeCacheMatrix <- function(currentMatrix = matrix()) {
+    #cached value (inverted matrix)
+    invertedMatrix <- NULL
+    
+    #matrix setter
+    set <- function(newMatrix) {
+        #store new matrix
+        currentMatrix <<- newMatrix
+        
+        #invalidate old inversion of the matrix (because currentMatrix has changed)
+        invertedMatrix <<- NULL
+    }
+    
+    #matrix getter
+    get <- function() {
+        currentMatrix
+    }
+    
+    #inverse matrix setter
+    setinv <- function(inv) {
+        invertedMatrix <<- inv
+    }
+    
+    #inverse matrix getter
+    getinv <- function() {
+        invertedMatrix
+    }
+    
+    #return list of functions
+    list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+#'cacheSolve' calls solve on special matrix ('solve' function is called only once for given matrix)
+#params:
+#   cacheMatrix - special matrix created by 'makeCacheMatrix' function
+#   ... - any other parameter will be passed to 'solve' function (it will only happen once - for given matrix)
+#result:
+#   inversion of matrix (result of 'solve' function)
+cacheSolve <- function(cacheMatrix, ...) {
+    
+    #get a matrix that is the inverse of 'cacheMatrix'
+    inv = cacheMatrix$getinv()
+    
+    if (is.null(inv)) {
+        #inversion not yet computed
+        
+        #get original matrix
+        mat = cacheMatrix$get()
+        
+        #calculate inversion
+        invMat = solve(mat, ...)
+        
+        #cache result
+        cacheMatrix$setinv(invMat)
+        
+        return(invMat)
+    }
+    else {
+        #inversion already calculated, nothing to do
+        return(inv)
+    }
 }
